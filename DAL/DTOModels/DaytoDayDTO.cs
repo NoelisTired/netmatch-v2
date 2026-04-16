0b3e739c5321867e16abbdcf39e88cc5e1980ad8
Odette <i579203@fontysict.nl>
2026-04-16 14:32:29 +0200

    Feature/DTOModels/DaytoDay/Quote

---
+++ DAL/DTOModels/DaytoDayDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTOModels
+{
+    public class DaytoDayDTO
+    {
+        public int Id {  get; set; }
+        public int DayNumber { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+
+        public DateTime Created_At { get; private set; }
+
+        public DaytoDayDTO(int id, int dayNumber, string title, string description)
+        {
+            this.Id = id;
+            this.DayNumber = dayNumber;
+            this.Title = title;
+            this.Description = description;
+        }
+
+    }
+}
---
+++ DAL/DTOModels/QuoteDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DAL.DTOModels
+{
+    public enum Status { Active, Inactive, Pending }
+    public enum Language { English }
+    
+    public class QuoteDTO
+    {
+        public int Id { get; private set; }
+        public string Title { get;private set; }
+        public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+        public QuoteDTO(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
+            
