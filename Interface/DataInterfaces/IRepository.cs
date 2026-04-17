f4c88d875ae109187af5caf7f2ce93fda7e79269
Odette <i579203@fontysict.nl>
2026-04-17 09:47:17 +0200

    Fix/Repository/toegevoegd

---
+++ Interface/DataInterfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Interface.DataInterfaces;
+
+public interface IRepository<T>
+{
+    Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+}
+
